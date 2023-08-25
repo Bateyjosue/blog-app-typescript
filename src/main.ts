219b833573bfe00b440d3fe9ff3804baf874b731
Bateyjosue <josuebatey19@gmail.com>
2023-08-25 20:59:12 +0200

    feat: setup taiwindcss

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import viteLogo from '/vite.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
+    <span class="flex justify-center">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+    </span>
     <h1>Vite + TypeScript</h1>
-    <h1>Blog App</h1>
+    <h1 class="text-3xl font-bold underline my-8 underline-offset-8">Blog App</h1>
   </div>
 `
