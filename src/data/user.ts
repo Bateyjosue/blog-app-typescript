9fbf67a8b9062d407e2b72518bcd490872fef044
Bateyjosue <josuebatey19@gmail.com>
2023-08-29 08:37:23 +0200

    feat: add user collection

---
+++ src/data/user.ts
@@ -0,0 +1,15 @@
+export interface User{
+    email:string,
+    fullName:string,
+    userName: string,
+    password: string
+}
+
+export  const users : User[] = [
+    {
+        email :'josuebatey19@gmail.com',
+        fullName: 'Josue Batey',
+        userName: 'josuebatey',
+        password: '000000'
+    }
+]
