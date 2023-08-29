f54f57d887ee61abb9233bd5247d2d6465184e07
Bateyjosue <josuebatey19@gmail.com>
2023-08-29 08:37:45 +0200

    feat: add posts collection

---
+++ src/data/posts.ts
@@ -0,0 +1,20 @@
+import {User, users} from './user'
+
+
+export interface Post {
+    title: string,
+    description: string,
+    pubDate: string,
+    tags: string[],
+    author: User
+}
+
+export const posts :Post[] = [
+    {
+        title: 'How Apple’s M1 Chips Make macOS Developement Much Less Costly',
+        description: 'Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        pubDate: 'June 09, 2023 - 01:10 PM',
+        tags: ['Development', 'Design'],
+        author: users[0]
+    }
+]
