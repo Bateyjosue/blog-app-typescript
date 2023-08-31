77daacf92ed24a476812db2365ed8acf0033ba11
Bateyjosue <josuebatey19@gmail.com>
2023-08-31 12:47:44 +0200

    feat: add new card in post

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -16,5 +16,26 @@ export const posts :Post[] = [
         pubDate: 'June 09, 2023 - 01:10 PM',
         tags: ['Development', 'Design'],
         author: users[0]
+    },
+    {
+        title: 'How Apple’s M1 Chips Make macOS Developement Much Less Costly',
+        description: 'Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        pubDate: 'June 09, 2023 - 01:10 PM',
+        tags: ['Development', 'Design'],
+        author: users[0]
+    },
+    {
+        title: 'How Apple’s M1 Chips Make macOS Developement Much Less Costly',
+        description: 'Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        pubDate: 'June 09, 2023 - 01:10 PM',
+        tags: ['Development', 'Design'],
+        author: users[0]
+    },
+    {
+        title: 'How Apple’s M1 Chips Make macOS Developement Much Less Costly',
+        description: 'Morem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.',
+        pubDate: 'June 09, 2023 - 01:10 PM',
+        tags: ['Development', 'Design'],
+        author: users[0]
     }
 ]
