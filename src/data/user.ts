519d743718c193b067698289e6b3fe2f28b0d24b
Bateyjosue <josuebatey19@gmail.com>
2023-09-07 12:08:25 +0200

    feat: fix spinner

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,15 +1,15 @@
-export interface User{
-    email:string,
-    fullName:string,
-    userName: string,
-    password: string
+export interface User {
+  email: string;
+  fullName: string;
+  userName: string;
+  password: string;
 }
 
-export  const users : User[] = [
-    {
-        email :'josuebatey19@gmail.com',
-        fullName: 'Josue Batey',
-        userName: 'josuebatey',
-        password: '000000'
-    }
-]
+export const users: User[] = [
+  {
+    email: "josuebatey19@gmail.com",
+    fullName: "Josue Batey",
+    userName: "josuebatey",
+    password: "000000",
+  },
+];
